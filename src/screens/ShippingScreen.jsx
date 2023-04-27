e78ae0c94113a98058709061051c702074382b48
trupti0406 <truptivijayyadav27@gmail.com>
2023-04-28 01:22:36 +0530

    first commit

---
+++ src/screens/ShippingScreen.jsx
@@ -0,0 +1,111 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Helmet } from "react-helmet-async";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import CheckOutBar from "../components/CheckOutBar";
+import { Store } from "../Store";
+
+export default function ShippingScreen() {
+  const navigate = useNavigate();
+  const { state, dispatch: contextDispatch } = useContext(Store);
+  const {
+    userInfo,
+    cart: { shippingAddress },
+  } = state;
+  const [fullName, setFullName] = useState(shippingAddress.fullName || "");
+  const [address, setAddress] = useState(shippingAddress.address || "");
+  const [city, setCity] = useState(shippingAddress.city || "");
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress.postalCode || ""
+  );
+  const [country, setCountry] = useState(shippingAddress.country || "");
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate("/signin?redirect=/shipping");
+    }
+  }, [userInfo, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    contextDispatch({
+      type: "SAVE_SHIPPING ADDRESS",
+      payload: {
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      },
+    });
+    localStorage.setItem(
+      "shippingAddress",
+      JSON.stringify({
+        fullName,
+        address,
+        city,
+        postalCode,
+        country,
+      })
+    );
+    navigate("/payment");
+    console.log("Navigated to next page");
+  };
+  return (
+    <>
+      <Helmet>
+        <title>Shipping Address</title>
+      </Helmet>
+      <CheckOutBar step1 step2></CheckOutBar>
+      <h2 className="mb-3 fw-bold text-center">Shipping Address</h2>
+      <form onSubmit={submitHandler} className="shipping-form mt-3 mb-4">
+        <Form.Group className="mb-3" controlId="fullName">
+          <Form.Label className='fw-semibold'>Full Name</Form.Label>
+          <Form.Control
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="address">
+          <Form.Label className='fw-semibold'>Address</Form.Label>
+          <Form.Control
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="city">
+          <Form.Label className='fw-semibold'>City</Form.Label>
+          <Form.Control
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="postalCode">
+          <Form.Label className='fw-semibold'>Postal Code</Form.Label>
+          <Form.Control
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="country">
+          <Form.Label className='fw-semibold'>Country</Form.Label>
+          <Form.Control
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <div className="d-grid">
+          <button id="login-btn" className="fw-bold">
+            Continue
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
